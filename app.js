ba66eeec500143a16c1db3a2881184047dc599b4
MohammedZakhbat <zakhbat.med@gmail.com>
2026-05-15 10:24:07 +0200

    add tailwindcss

---
+++ app.js
@@ -0,0 +1,7 @@
+function verificarPrimo(){
+    let n = parseInt(document.getElementById("numero").value)
+    let resultado = document.getElementById("resultado")
+
+    resultado.className="text-center text-lg font-semibold p-4 min-h-[60px] flex items-center justify-center rounded-2xl border";
+    
+}
